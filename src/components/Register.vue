1dd03673332d1c7737405bc24492ddefde36bedf
Cbear1234 <amywonderland77@gmail.com>
2022-01-05 17:57:54 +0900

    dd

---
+++ src/components/Register.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h2>Register</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Register",
+  data() {},
+  methods: {},
+};
+</script>
+<style lang=""></style>
